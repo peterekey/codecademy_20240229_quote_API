07e0c72980182220097834734e1860a503f4914f
Peter Key <63628694+peterekey@users.noreply.github.com>
2024-03-01 08:54:43 +1100

    Step 4: Make a GET /api/quotes/random route for the API

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const { quotes } = require('./data');
 const { getRandomElement } = require('./utils');
 
 const PORT = process.env.PORT || 4001;
-
 app.use(express.static('public'));
+
+app.get('/api/quotes/random', (req, res, next) => {
+    const quoteToSend = getRandomElement(quotes)
+    res.send({quote: quoteToSend})
+})
 
 app.listen(PORT, () => console.log('Listening on 4001...'))
